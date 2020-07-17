fddb9febbb724bbed46924b10ff046ef200118b7
chenli09 <kison.leec@gmail.com>
2020-07-17 13:47:41 +0800

    refactor

--- README.md
+++ README.md
@@ -10,30 +10,32 @@
 ## 2. 代码结构
 ```
 .
-├── eda                        # EDA算法实现
-│   ├── data                   # 原始数据路径，以及其它数据 
-│   ├── utils
-│       ├── eda_gen.py         # 输入一条句子，返回增广后的句子集
-│   └── augment.py             # 主程序，扩充原始数据集
-├── models                     # 验证增强数据效果的模型
-│   ├── textCNN                # CNN算法实现
-├── main.py                    # 验证不同增广算法生成的数据对分类模型的效果
+├── eda                          # EDA算法实现  
+│   ├── eda_gen.py               # 输入一条句子，返回增广后的句子集
+├── data                         # 各类数据路径
+│   ├── ori_data                 # 原始数据存储路径
+│   ├── aug_data                 # 增强数据存储路径
+│   ├── stopwords                # 停用词表存储路径
+├── experiments                  # 验证不同增广算法对分类模型的效果
+│   ├── models                   # 不同模型的实现
+│   ├── main.py                  # 训练和验证，结果展示
+├── augment.py                   # 主程序，扩充原始数据集 
 ├── README.md
-└── requirements.txt           # 第三方库依赖
+└── requirements.txt             # 第三方库依赖
 ```
 
 ## 3. 增强脚本
 ### 3.1 EDA
 ```
-cd eda/
-python augment.py --input data/weather_data.txt --output data/eda_weather_data.txt --num_aug 9 --alpha 0.1
-# 后三个参数可省略
+cd LowResource_data_aug/
+python augment.py --method eda --input_file data/ori_data/weather_data.txt --output data/aug_data/ --num_aug 9 --alpha 0.1
+# 后两个参数可省略
 ```
 
 ## 4. 效果验证脚本
 ### 4.1 EDA
 ```
-python main.py --aug_data eda/data/eda_weather_data.txt --model textCNN
+python experiments/main.py --aug_data data/aug_data/eda_weather_data.txt --model textCNN
 ```
 - 固定同一个原始数据集，使用不同方法获得增强后数据集，并通过同一个分类模型训练验证
 - 使用不同分类模型时，同一个方法的增益差异
--- augment.py
+++ augment.py
@@ -9,38 +9,35 @@
 @Desc    :
 """
 
-from utils import eda_gen
+from eda import eda_gen
 import argparse
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input', required=True, type=str, help='原始数据的文件路径')
-parser.add_argument('--output', required=False, type=str, help='增强数据的输出文件路径')
+parser.add_argument('--method', required=True, type=str, help='增强方法选择')
+parser.add_argument('--input_file', required=True, type=str, help='原始数据的文件路径')
+parser.add_argument('--output', required=True, type=str, help='增强数据的输出路径')
 parser.add_argument('--num_aug', required=False, type=int, default=9, help='每条原始语句增强的语句数')
 parser.add_argument('--alpha', required=False, type=float, default=0.1, help='每条语句中将会被改变的单词数占比')
 args = parser.parse_args()
 
 num_aug = args.num_aug
 alpha = args.alpha
-output_path = None
-if args.output:
-    output_path = args.output
-else:
-    dir_name = os.path.dirname(args.input)
-    file_name = 'eda_' + os.path.basename(args.input)
-    output_path = os.path.join(dir_name, file_name)
+file_name = args.method + '_' + os.path.basename(args.input_file)
+output_file = os.path.join(args.output, file_name)
 
 
-def augment(original_data, output_file, n_aug, p_change):
-    writer = open(output_file, 'w')
-    print("正在使用EDA生成增强语句...")
+def augment(method, original_data, o_file, n_aug, p_change):
+    writer = open(o_file, 'w')
+    print("正在使用{}生成增强语句...".format(method))
     with open(original_data, 'r') as f:
         for line in f:
             line = line.strip()
             parts = line.split('\t')
             label = parts[0]
             sentence = parts[1]
-            aug_sentences = eda_gen.eda(sentence, p_change, p_change, p_change, p_change, n_aug)
+            if method == 'eda':
+                aug_sentences = eda_gen.eda(sentence, p_change, p_change, p_change, p_change, n_aug)
             for aug_sentence in aug_sentences:
                 writer.write(label + '\t' + aug_sentence + '\n')
     writer.close()
@@ -49,4 +46,4 @@ def augment(original_data, output_file, n_aug, p_change):
 
 
 if __name__ == '__main__':
-    augment(args.input, output_path, num_aug, alpha)
+    augment(args.method, args.input_file, output_file, num_aug, alpha)
